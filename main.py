6f99837645b364d6fdca0e504d4fd86a60115bcb
Fabrizio Ruocco <ruoccofabrizio@gmail.com>
2021-03-02 16:39:40 +0000

    fixed dst index

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def get_next_chunk(current_val):
 
 
 # Get all documents in a batch [low_b, high_b]
-def get_all_docs(low_b, high_b, filter_by= "Id"):
+def get_all_docs(low_b, high_b, src_endpoint, src_headers, filter_by= "Id"):
 
     documents = []
 
     searchstring = f"&$filter={filter_by} gt '{low_b}' and {filter_by} le '{high_b}'&$count=true"
-    url = endpoint + "indexes/" + src_index +"/docs" + api_version + searchstring
-    response  = requests.get(url, headers=headers, json=searchstring)
+    url = src_endpoint + "indexes/" + src_index +"/docs" + api_version + searchstring
+    response  = requests.get(url, headers=src_headers, json=searchstring)
     query = response.json()
 
     if query.get('value') != None:
@@ -29,7 +29,7 @@ def get_all_docs(low_b, high_b, filter_by= "Id"):
             next_link = query['@odata.nextLink']
             #print(next_link)
 
-            response = requests.get(next_link, headers=headers)
+            response = requests.get(next_link, headers=src_headers)
             query = response.json()
 
             for doc in query.get('value'):
@@ -40,20 +40,20 @@ def get_all_docs(low_b, high_b, filter_by= "Id"):
     return documents
 
 
-def push_docs(all_documents):
+def push_docs(all_documents, dst_endpoint, dst_headers):
     # Push data
     batch_index = 0
     while batch_index * 50 < len(all_documents):
         batch_start = batch_index * 50
         batch_end = (batch_index + 1) * 50 if (batch_index + 1) * 50 < len(all_documents) else len(all_documents)
         logging.info(f"Pushing batch #{batch_index} [{batch_start},{batch_end}]")
-        push_batch(all_documents[batch_start:batch_end])
+        push_batch(all_documents[batch_start:batch_end], dst_endpoint, dst_headers)
         batch_index += 1
 
         # print(index_content)
 
 
-def push_batch(batch_documents):
+def push_batch(batch_documents, dst_endpoint, dst_headers):
 
     search_docs = {
         "value" : []
@@ -67,10 +67,10 @@ def push_batch(batch_documents):
 
     # search_docs['value'][0]
 
-    url = endpoint + "indexes/" + dst_index + '/docs/index' + api_version
-    response = requests.post(url, headers = headers, json = search_docs)
+    url = dst_endpoint + "indexes/" + dst_index + '/docs/index' + api_version
+    response = requests.post(url, headers = dst_headers, json = search_docs)
     index_content = response.json()
-
+    logging.info(response.status_code)
 
 
 # MAIN
@@ -115,16 +115,20 @@ if __name__ == "__main__":
 
     logging.info(f"Copying :\n\tSOURCE SERVICE: {src_service} INDEX: {src_index}\n\tDESTINATION SERVICE {dst_service} INDEX: {dst_index}\n\tFiltering by: {filter_by}")
 
-    endpoint = 'https://' + src_service + '.search.windows.net/'
+    src_endpoint = 'https://' + src_service + '.search.windows.net/'
+    dst_endpoint = 'https://' + dst_service + '.search.windows.net/'
+
     api_version = '?api-version=2019-05-06'
-    headers = {'Content-Type': 'application/json',
+    src_headers = {'Content-Type': 'application/json',
             'api-key': src_service_key } 
+    dst_headers = {'Content-Type': 'application/json',
+            'api-key': dst_service_key } 
 
 
     searchstring = '&search=*&$count=true'
 
-    url = endpoint + "indexes/" + src_index +"/docs" + api_version + searchstring
-    response  = requests.get(url, headers=headers, json=searchstring)
+    url = src_endpoint + "indexes/" + src_index +"/docs" + api_version + searchstring
+    response  = requests.get(url, headers=src_headers, json=searchstring)
     query = response.json()
     #print(query)
 
@@ -134,22 +138,22 @@ if __name__ == "__main__":
 
     # Get and push docs
 
-    val = '0'
+    val = '/'
     while (ord(val) <= 123): #123
         val, low_b, high_b = get_next_chunk(val)
         logging.info(f" Processing interval : [{low_b},{high_b}]")
         # val = chr(ord(val) + 1)
         documents = []
-        documents = get_all_docs(low_b, high_b, filter_by=filter_by)
+        documents = get_all_docs(low_b, high_b, src_endpoint, src_headers, filter_by=filter_by)
         logging.info(len(documents))
-        push_docs(documents)
+        push_docs(documents, dst_endpoint, dst_headers)
 
 
     # Check
     searchstring = '&search=*&$count=true'#&$orderby=metadata_storage_last_modified desc'
 
-    url = endpoint + "indexes/" + dst_index +"/docs" + api_version + searchstring
-    response  = requests.get(url, headers=headers, json=searchstring)
+    url = dst_endpoint + "indexes/" + dst_index +"/docs" + api_version + searchstring
+    response  = requests.get(url, headers=dst_headers, json=searchstring)
     query = response.json()
 
 
